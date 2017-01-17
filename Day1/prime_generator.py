05d0b44652fe30834c6f00b557e836fb5b9e1e2e
maryanne <ngangamaryanne@github.com>
2017-01-17 11:05:00 +0300

    prime numbers generator and the testing file with 10 tests

---
+++ Day1/prime_generator.py
@@ -0,0 +1,17 @@
+def gen_prime(n):
+  prime_numbers=[]
+  if isinstance(n,int):
+    if n>1:
+      for num in range(2, n+1):
+        for i in range (2, num):
+    	    if (num%i==0):
+    				break
+        else:
+        	prime_numbers.append(num)
+      return prime_numbers
+    else:
+      raise ValueError("Please input a positive integer")
+  else:
+    raise TypeError("Please input an integer")
+
+      
