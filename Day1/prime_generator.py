2ea584df6e2f888ede84c24c89daddd30737eaf1
maryanne <ngangamaryanne@github.com>
2017-01-18 15:29:54 +0300

    Updated prime generator with the testing file

--- Day1/prime_generator.py
+++ Day1/prime_generator.py
@@ -1,17 +1,15 @@
 def gen_prime(n):
-  prime_numbers=[]
-  if isinstance(n,int):
+  prime_numbers = []
+  if isinstance (n, int):
     if n>1:
-      for num in range(2, n+1):
+      for num in range (2, n+1):
         for i in range (2, num):
-    	    if (num%i==0):
-    				break
+          if (num%i==0):
+            break
         else:
-        	prime_numbers.append(num)
+          prime_numbers.append(num)
       return prime_numbers
     else:
-      raise ValueError("Please input a positive integer")
+      raise ValueError ('Please input a positive integer')
   else:
-    raise TypeError("Please input an integer")
-
-      
+    raise TypeError ('please input an integer')
--- Day1/prime_test.py
+++ Day1/prime_test.py
@@ -1,1 +1,40 @@
-import unitest
+import unittest
+from prime_generator import gen_prime
+
+class Prime_Generator_test(unittest.TestCase):
+	def test_generates_primes(self):
+		self.assertEqual(gen_prime(7), [2,3,5,7])
+
+	def test_Raises_error_for_input_zero(self):
+		with self.assertRaises(ValueError):gen_prime(0)
+
+	def test_Raises_error_for_negative_input(self):
+		with self.assertRaises(ValueError):gen_prime(-1)
+
+	def test_Raises_error_for_dict_input(self):
+		with self.assertRaises(TypeError):gen_prime({})
+
+	def test_generates_one_prime_for_input_2(self):
+		self.assertEqual(gen_prime(2), [2])
+
+	def test_Raises_error_for_float_input(self):
+		with self.assertRaises(TypeError):gen_prime(4.5)
+
+	def test_Raises_error_for_list_input(self):
+		with self.assertRaises(TypeError):gen_prime([2])
+
+	def test_raises_error_for_no_input(self):
+		with self.assertRaises (TypeError):gen_prime(None)
+	def test_input_is_number(self):
+		self.assertIsInstance(gen_prime(9), list)
+
+	def test_Raises_error_for_basstring_input(self):
+		with self.assertRaises(TypeError):gen_prime("hello")
+
+
+
+
+
+
+if __name__=='__main__':
+	unittest.main()
