0a3c56ffcd176df516127d477d08c6926d53b485
maryanne <ngangamaryanne@github.com>
2017-01-18 13:48:13 +0300

    corrections on car_class.py lab

--- Day2/Andelabs/car_class.py
+++ Day2/Andelabs/car_class.py
@@ -1,35 +1,36 @@
+
 class Car (object):
   
-  num_of_wheels=None
-  num_of_doors = None
-  speed = 0
-
-  def __init__(self, *argv):
-    self.name= "General"
-    self.model = "GM"
-    #self.car_type = car_type
-    if self.name == 'Porshe' or self.name =='Koenigsegg':
+  def __init__( self, name='General', model = 'GM', car_type= None, speed = 0):
+    self.name=name
+    self.model = model
+    self.speed = speed
+    self.car_type = car_type
+    
+    if self.name == 'Porshe' or self.name=='Koenigsegg':
       self.num_of_doors = 2
     else:
-      num_of_doors =4
-    for arg in argv:
-      if arg=='Trailer':
-        self.num_of_wheels = 8
-      else:
-        self.num_of_wheels = 4
+      self.num_of_doors = 4
       
-    
-      
-      
-  def drive(self, gas):
-    if gas>0:
-      speed = 100
-      return gas *speed
+    if self.car_type =='trailer':
+      self.num_of_wheels = 8
     else:
-      return Car.speed
-    
+      self.num_of_wheels = 4
+      
   def is_saloon(self):
-    if self.car_type !='Trailer':
-      return 'Saloon'
-  
+    if self.car_type != 'trailer':
+      self.car_type == 'saloon'
+      return True
+    else:
+      return False
+
+  def drive (self,moving_speed):
+    if self.car_type =='trailer':
+      self.speed = 77
+      
+    if self.name=='Mercedes':
+      self.speed = 10**moving_speed
     
+    return self
+    
+ 
