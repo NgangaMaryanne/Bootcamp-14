380e34e85bd3b52b7e6dd6e3bbaf0f011009fd27
maryanne <ngangamaryanne@github.com>
2017-01-23 23:24:10 +0300

    minor changes to  code

--- Week2/my-kanban-app/kanban_app.py
+++ Week2/my-kanban-app/kanban_app.py
@@ -3,17 +3,18 @@ import sqlite3
 
 class KanbanApp(cmd.Cmd):
     #code for all functions and commands
-    conn = sqlite3.connect('kanban_app.db')
-    c = conn.cursor()
+
     def __init__(self):
         super(KanbanApp, self).__init__()
 
     def do_todo(self,item_name):
+        conn = sqlite3.connect('kanban_app.db')
+        c = conn.cursor()
         item_name = input("input new item: ")
-        self.c.execute("INSERT INTO task VALUES(NULL,?);", [item_name])
-        self.c.execute("")
-        self.conn.commit()
-        self.conn.close()
+        c.execute("INSERT INTO task VALUES(NULL,?);", [item_name])
+        c.execute("")
+        conn.commit()
+        conn.close()
 
 
 
